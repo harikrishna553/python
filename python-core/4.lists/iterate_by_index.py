a13035e01f31eea645ffc05a484123dbcdc3a374
harikrishna553 <harikrishna553@gmail.com>
2024-02-01 16:29:21 +0530

    Add more examples

---
+++ python-core/4.lists/iterate_by_index.py
@@ -0,0 +1,4 @@
+primes = [2, 3, 5, 7, 11]
+
+for index in range(len(primes)):
+    print(f'{index}. {primes[index]}')
---
+++ python-core/4.lists/iterate_ele_by_ele.py
@@ -0,0 +1,4 @@
+primes = [2, 3, 5, 7, 11]
+
+for ele in primes:
+    print(ele)
