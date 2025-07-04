19a1a28a4863bae1bb08622beeee4e7dfff84add
Harikrishna <harikrishna.gurram@walmart.com>
2025-07-04 16:13:00 +0530

    Create Documents from Splitteer create_documents method

---
+++ langchain/langchain-demo/6.core_components/6.2.document_splitters/pass_metadata_for_create_docs.py
@@ -0,0 +1,21 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text = """
+LangChain is a framework for developing applications powered by large language models (LLMs).
+
+LangChain simplifies every stage of the LLM application lifecycle:
+
+Development: Build your applications using LangChain's open-source components and third-party integrations. Use LangGraph to build stateful agents with first-class streaming and human-in-the-loop support.
+Productionization: Use LangSmith to inspect, monitor and evaluate your applications, so that you can continuously optimize and deploy with confidence.
+Deployment: Turn your LangGraph applications into production-ready APIs and Assistants with LangGraph Platform.
+"""
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=10)
+
+# input is a list of raw strings
+docs = text_splitter.create_documents([text], metadatas=[{"source": "LangChain Documentation"}])
+
+print(f"Type Of Document : {type(docs[0])}")  # <class 'langchain_core.documents.base.Document'>
+print(f"Total Documents : ' + {len(docs)}")
+print(f"First Page Content: {docs[0].page_content}")
+print(f"Metadata: {docs[0].metadata}")
